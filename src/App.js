be4c00ff78fd076e00e176739c1b38a31c0a2d88
Anish Chaurasia <anishraut2111@gmail.com>
2023-07-13 18:35:20 +0530

    abc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import projectData from './projectData';
+import { IoIosArrowDropdown, IoIosArrowDropup } from "react-icons/io";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [data, setData] = useState(projectData);
+
+
+
+  function showing(x) {
+    const newdata = data.map((obj, ind) => {
+      if (ind === x) {
+        if (obj.id === true) {
+          return { ...obj, id: false }
+        }
+        else {
+          return { ...obj, id: true }
+        }
+      }
+      return obj;
+    });
+
+    setData(newdata);
+  }
+
+
+
+
+  return (<div className="outercontainer">
+    <div className="oitem" id='oitem1'><h3>Questions And Answer About React js</h3></div>
+    <div className="oitem" id='oitem2'>
+
+
+
+
+      {
+        data.map((item, index) => {
+          return (
+            <div className='innerItem'>
+              <div className="inneritems">
+                <h5 id='paraqstn'>{item.question}</h5>
+                {item.id === false && <button key={index} type='button' onClick={() => showing(index)}><IoIosArrowDropdown /></button>}
+                {item.id === true && <button key={index} type='button' id='afterclk' onClick={() => showing(index)}><IoIosArrowDropup /></button>}
+                {item.id && <p>{item.answer}</p>}
+              </div>
+
+            </div>
+          )
+        })
+      }
     </div>
-  );
+  </div>
+
+
+  )
 }
 
 export default App;
---
+++ src/projectData.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+
+const projectData = [{
+    id: false,
+    question: "What is an event in React?",
+    answer: "An event is an action that a user or system may trigger, such as pressing a key, a mouse click, etc."
+},
+
+{
+    id: false,
+    question: "Why is there a need for using keys in Lists?",
+    answer: "A key is a unique identifier and it is used to identify which items have changed, been updated or deleted from the lists."
+},
+
+{
+    id: false,
+    question: "What are forms in React?",
+    answer: "React employs forms to enable users to interact with web applications."
+},
+
+
+{
+    id: false,
+    question: "What are the components in React?",
+    answer: "Components are the building blocks of any React application, and a single app usually consists of multiple components."
+}
+];
+export default projectData;
